4f06e093d799d1ec2863f7268c3a7881bac83677
Vytautas Luksys <vytautas.luksys@cgi.com>
2024-08-13 18:02:36 +0300

    feat: radio button group component

---
+++ projects/rc-ses/angular-components/src/lib/components/form/input/radio-button-group/radio-button-group.ts
@@ -0,0 +1,93 @@
+import { CommonModule, DOCUMENT } from "@angular/common";
+import { Component, AfterViewInit, OnDestroy, ElementRef, Inject, Renderer2, Input, ContentChildren, QueryList, model, OnChanges, SimpleChanges, OnInit, forwardRef, ViewChild, Injector, booleanAttribute } from "@angular/core";
+import { ControlContainer, ControlValueAccessor, FormControl, FormControlDirective, FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCommonModule, MatOption } from "@angular/material/core";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatRadioButton, MatRadioModule } from "@angular/material/radio";
+import { ButtonDirective } from "../../../button/button";
+
+type Option = { value: string, label: string };
+type Options = Option[];
+
+@Component({
+  selector: 'rc-ses-radio-button-group',
+  standalone: true,
+  templateUrl: 'radio-button-group.html',
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    MatButtonModule,
+    MatCommonModule,
+    MatFormFieldModule,
+    MatRadioModule,
+
+    ButtonDirective,
+  ],
+  host: {
+    class: 'rc-ses-element rc-ses-radio-button-group'
+  },
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RadioButtonGroupComponent),
+      multi: true
+    }
+  ]
+})
+export class RadioButtonGroupComponent implements ControlValueAccessor, AfterViewInit, OnChanges, OnInit, OnDestroy {
+  @ContentChildren(MatRadioButton)
+  queryOptions!: QueryList<MatRadioButton>;
+
+  @ViewChild(FormControlDirective, {static: true})
+  formControlDirective: FormControlDirective | undefined;
+
+  @Input() formControl!: FormControl;
+  @Input() formControlName: string | undefined;
+  @Input() options!: Options;
+
+  initialized: boolean = false;
+
+  constructor(
+    @Inject(DOCUMENT) private _document: Document,
+    public _element: ElementRef,
+    private _renderer: Renderer2,
+    private injector: Injector,
+  ) {}
+
+  get controlContainer() {
+    return this.injector.get(ControlContainer);
+  }
+
+  get control() {
+    return this.formControl ||
+      (this.formControlName && this.controlContainer.control?.get(this.formControlName));
+  }
+
+  writeValue(obj: any): void {
+    this.formControlDirective?.valueAccessor?.writeValue(obj);
+  }
+
+  registerOnChange(fn: any): void {
+    this.formControlDirective?.valueAccessor?.registerOnChange(fn);
+  }
+
+  registerOnTouched(fn: any): void {
+    this.formControlDirective?.valueAccessor?.registerOnTouched(fn);
+  }
+
+  ngOnChanges(_changes: SimpleChanges): void {}
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.initialized = true;
+  }
+
+  ngOnDestroy() {
+    this.initialized = false;
+  }
+}
