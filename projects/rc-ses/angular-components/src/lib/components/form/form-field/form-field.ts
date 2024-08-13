e0be388f39108192e58c91466e034237b22f19e5
Vytautas Luksys <vytautas.luksys@cgi.com>
2024-08-13 18:02:07 +0300

    feat: form field component updates

--- projects/rc-ses/angular-components/src/lib/components/form/form-field/form-field.ts
+++ projects/rc-ses/angular-components/src/lib/components/form/form-field/form-field.ts
@@ -1,7 +1,7 @@
 import { CommonModule, DOCUMENT } from "@angular/common";
-import { Component, AfterViewInit, OnDestroy, ElementRef, Inject, OnInit, Renderer2, Input, ContentChild, signal } from "@angular/core";
-import { FormControl, FormsModule } from "@angular/forms";
-import { MatError, MatFormField, MatFormFieldControl, MatFormFieldModule } from "@angular/material/form-field";
+import { Component, AfterViewInit, OnDestroy, ElementRef, Inject, OnInit, Renderer2, Input, ContentChild, signal, AfterContentInit, ViewChild } from "@angular/core";
+import { ControlContainer, FormControl, FormGroupDirective, FormsModule } from "@angular/forms";
+import { MatError, MatFormField, MatFormFieldControl, MatFormFieldModule, MatLabel } from "@angular/material/form-field";
 import { MatInput, MatInputModule } from "@angular/material/input";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
@@ -25,36 +25,25 @@ import { InputDirective } from "../input/input/input";
   host: {
     class: 'rc-ses-element rc-ses-form-field'
   },
+  viewProviders: [
+    { provide: ControlContainer, useExisting: FormGroupDirective }
+  ],
 })
-export class FormField implements AfterViewInit, OnDestroy, OnInit {
+export class FormFieldComponent implements AfterViewInit, AfterContentInit, OnDestroy, OnInit {
   @ContentChild(MatFormFieldControl) control!: MatFormFieldControl<any>;
 
-  @ContentChild(MatFormField, { read: ElementRef }) formFieldRef!: ElementRef<MatFormField>;
+  @ContentChild(MatFormField, { read: ElementRef}) formFieldRef!: ElementRef<MatFormField>;
   @ContentChild(MatFormField) formField!: MatFormField;
 
-  @ContentChild(MatInput, { read: ElementRef }) inputRef!: ElementRef<MatInput>;
+  @ContentChild(MatInput, { read: ElementRef}) inputRef!: ElementRef<MatInput>;
   @ContentChild(MatInput) input!: MatInput;
+
+  @ViewChild(MatLabel, { read: ElementRef }) labelComponent: MatLabel | undefined;
 
   @Input() label: string | undefined = undefined;
   @Input() labelSubtitle: string | undefined = undefined;
 
-  @Input() formControl: FormControl | undefined = undefined;
-
   public initialized: boolean = false;
-
-  errorMessage = signal('');
-
-  updateErrorMessage() {
-    if (!this.formControl) return;
-
-    if (this.formControl.hasError('required')) {
-      this.errorMessage.set('You must enter a value');
-    } else if (this.formControl.hasError('email')) {
-      this.errorMessage.set('Not a valid email');
-    } else {
-      this.errorMessage.set('');
-    }
-  }
 
   constructor(
     @Inject(DOCUMENT) private _document: Document,
@@ -63,12 +52,13 @@ export class FormField implements AfterViewInit, OnDestroy, OnInit {
   ) {}
 
   ngOnInit() {
-    if (this.labelSubtitle) {
+    if (this.labelSubtitle)
       this._renderer.addClass(this._element.nativeElement, 'rc-ses-has-label-subtitle');
-    }
   }
 
-  ngAfterViewInit() {
+  ngAfterViewInit() {}
+
+  ngAfterContentInit(): void {
     if (this.input?.readonly) {
       this._renderer.addClass(this.formField._elementRef.nativeElement, 'mat-form-field-readonly');
       this._renderer.addClass(this.formFieldRef.nativeElement, 'mat-form-field--readonly');
