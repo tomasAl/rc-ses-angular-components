360618bccd4d56e1e62e89ed41eca623f8e0670d
Vytautas Luksys <vytautas.luksys@cgi.com>
2024-08-13 18:03:19 +0300

    feat: new components in sample form

--- projects/app/src/app/app.component.ts
+++ projects/app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, viewChild } from '@angular/core';
+import { Component,  viewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
@@ -12,15 +12,19 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
 
 import { AccordionDirective } from '../../../rc-ses/angular-components/src/lib/components/accordion/accordion';
 import { ButtonDirective } from '../../../rc-ses/angular-components/src/lib/components/button/button';
-import { FormField } from '../../../rc-ses/angular-components/src/lib/components/form/form-field/form-field';
+import { FormFieldComponent } from '../../../rc-ses/angular-components/src/lib/components/form/form-field/form-field';
 import { InputDirective } from '../../../rc-ses/angular-components/src/lib/components/form/input/input/input';
-// import { ServiceFormActions } from '../../../rc-ses/angular-components/src/lib/components/layout/service-form-actions/service-form-actions';
 import { ServiceFormContainer } from '../../../rc-ses/angular-components/src/lib/components/layout/service-form-container/service-form-container';
 import { ServiceHeader } from '../../../rc-ses/angular-components/src/lib/components/layout/service-header/service-header';
 import { ServicePage } from '../../../rc-ses/angular-components/src/lib/components/layout/service-page/service-page';
 import { ServiceWizardSteps } from '../../../rc-ses/angular-components/src/lib/components/layout/service-wizard-steps/service-wizard-steps';
 import { MatSelectModule } from '@angular/material/select';
-import { SelectComponent } from '../../../rc-ses/angular-components/src/lib/components/form/select/select';
+import { SelectComponent } from '../../../rc-ses/angular-components/src/lib/components/form/input/select/select';
+import { NumberStepperComponent } from '../../../rc-ses/angular-components/src/lib/components/form/input/number-stepper/number-stepper';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { AgreementCheckboxComponent } from '../../../rc-ses/angular-components/src/lib/components/form/input/agreement-checkbox/agreement-checkbox';
+import { MessageComponent } from '../../../rc-ses/angular-components/src/lib/components/message/message';
+import { RadioButtonGroupComponent } from '../../../rc-ses/angular-components/src/lib/components/form/input/radio-button-group/radio-button-group';
 
 @Component({
   selector: 'app-root',
@@ -33,6 +37,7 @@ import { SelectComponent } from '../../../rc-ses/angular-components/src/lib/comp
 
     MatButtonModule,
     MatCommonModule,
+    MatCheckboxModule,
     MatExpansionModule,
     MatFormFieldModule,
     MatIconModule,
@@ -40,9 +45,13 @@ import { SelectComponent } from '../../../rc-ses/angular-components/src/lib/comp
     MatSelectModule,
 
     AccordionDirective,
+    AgreementCheckboxComponent,
     ButtonDirective,
-    FormField,
+    FormFieldComponent,
     InputDirective,
+    MessageComponent,
+    NumberStepperComponent,
+    RadioButtonGroupComponent,
     SelectComponent,
 
     ServiceHeader,
@@ -59,20 +68,60 @@ import { SelectComponent } from '../../../rc-ses/angular-components/src/lib/comp
 export class AppComponent {
   accordion = viewChild.required(MatAccordion);
 
-  form = new FormGroup({
-    tikslasCustom: new FormControl(''),
+  formStep1 = new FormGroup({
+    purpose: new FormControl(
+      { value: '', disabled: false },
+      { validators: Validators.required },
+    ),
+    legalBasis: new FormControl(
+      { value: '', disabled: false },
+      { validators: Validators.required },
+    ),
+    propertyAddress: new FormControl(
+      { value: '', disabled: false },
+      { validators: Validators.required },
+    ),
+    agreement: new FormControl(
+      { value: false, disabled: false },
+      { validators: Validators.required },
+    ),
+    deadline: new FormControl(
+      { value: '', disabled: false },
+      { validators: Validators.required },
+    ),
+    numberOfCopies: new FormControl(
+      { value: '', disabled: false },
+      { validators: Validators.required },
+    ),
   });
-  tikslasCustom = new FormControl('');
 
-  readonly testInputFormControl = new FormControl('', [Validators.required]);
-
-  value = 'Tekstas';
-
-  purposeModel: string = '';
   purposeOptions: { value: string, label: string, description?: string }[] = [
     { value: 'tikslas1', label: 'Tikslas 1' },
     { value: 'tikslas2', label: 'Tikslas 2' },
     { value: 'tikslas3', label: 'Tikslas 3' },
+  ];
+
+  legalBasisOptions: { value: string, label: string, description?: string }[] = [
+    { value: 'teisinisPagrindas1', label: 'Teisinis pagrindas 1' },
+    { value: 'teisinisPagrindas2', label: 'Teisinis pagrindas 2' },
+    { value: 'teisinisPagrindas3', label: 'Teisinis pagrindas 3' },
+  ];
+
+  deadlineOptions: { value: string, label: string, description?: string }[] = [
+    { value: 'terminas-1', label: 'artimiausiu metu' },
+    { value: 'terminas-2', label: 'iki mėnesio pabaigos' },
+  ];
+
+  propertyAddressOptions: { value: string, label: string, description?: string }[] = [
+    { value: 'lvivo-g-7', label: 'Lvivo g. 7', description: 'Vilnius, Vilniaus m. sav. 09313' },
+    { value: 'lvivo-g-9', label: 'Lvivo g. 9', description: 'Vilnius, Vilniaus m. sav. 09313' },
+    { value: 'lvivo-g-11', label: 'Lvivo g. 11', description: 'Vilnius, Vilniaus m. sav. 09313' },
+    { value: 'lvivo-g-11a', label: 'Lvivo g. 11A', description: 'Vilnius, Vilniaus m. sav. 09313' },
+    { value: 'lvivo-g-12', label: 'Lvivo g. 12', description: 'Vilnius, Vilniaus m. sav. 09313' },
+    { value: 'lvivo-g-13', label: 'Lvivo g. 13', description: 'Vilnius, Vilniaus m. sav. 09313' },
+    { value: 'lvivo-g-13a', label: 'Lvivo g. 13A', description: 'Vilnius, Vilniaus m. sav. 09313' },
+    { value: 'lvivo-g-13b', label: 'Lvivo g. 13B', description: 'Vilnius, Vilniaus m. sav. 09313' },
+    { value: 'lvivo-g-1c', label: 'Lvivo g. 13C', description: 'Vilnius, Vilniaus m. sav. 09313' },
   ];
 
   purposeOptionsWithDescription: { value: string, label: string, description?: string }[] = [
@@ -99,6 +148,65 @@ export class AppComponent {
         { value: 'tikslas5', label: 'Tikslas 5' },
         { value: 'tikslas6', label: 'Tikslas 6' },
       ],
+    },
+  ];
+
+
+  formStep2 = new FormGroup({
+    issueMethod: new FormControl(
+      { value: '', disabled: false },
+      { validators: Validators.required },
+    ),
+    rcBranch: new FormControl(
+      { value: '', disabled: false },
+      { validators: Validators.required },
+    ),
+  });
+
+  rcBranchOptions: { value: string, label: string, description?: string }[] = [
+    {
+      label: 'Lvivo g. 25-101',
+      value: 'vilnius',
+      description: '09320 Vilnius',
+    },
+    {
+      label: 'E. Ožeškienės g. 12',
+      value: 'kaunas',
+      description: '44252 Kaunas, (įėjimas iš L. Sapiegos gatvės pusės)',
+    },
+    {
+      label: 'Baltijos pr. 123-1',
+      value: 'klaipeda',
+      description: '93224 Klaipėda',
+    },
+    {
+      label: 'Baltijos pr. 123-1',
+      value: 'siauliai',
+      description: '78167 Šiauliai',
+    },
+    {
+      label: 'Baltijos pr. 123-1',
+      value: 'panevezys',
+      description: '35173 Panevėžys',
+    },
+  ];
+
+  issueMethodOptions: { value: string, label: string }[] = [
+    {
+      label: 'El. paštu',
+      value: 'email',
+    },
+    {
+      label: 'Padalinyje',
+      value: 'branch',
+    },
+    {
+      label: 'Paštu Lietuvoje',
+      value: 'mail-local',
+    },
+    {
+      label: 'Paštu užsienyje',
+      value: 'mail-abroad',
     },
   ];
 }
