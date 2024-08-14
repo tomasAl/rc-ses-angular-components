f6afd599683935f2495a9cdef76990f7b9a0d9d9
Vytautas Luksys <vytautas.luksys@cgi.com>
2024-08-14 13:59:59 +0300

    feat: routing setup

--- projects/app/src/app/app.routes.ts
+++ projects/app/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent : () => import('./landing-page/landing-page').then(m => m.LandingPageComponent),
+  },
+  {
+    path: 'sample-form-multiple-steps',
+    loadComponent : () => import('./sample-form-multiple-steps/sample-form-multiple-steps').then(m => m.SampleFormMultipleStepsComponent),
+  },
+  {
+    path: 'sample-form-single-step',
+    loadComponent : () => import('./sample-form-single-step/sample-form-single-step').then(m => m.SampleFormSingleStepComponent),
+  },
+];
